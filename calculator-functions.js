6ae462d6600cee62326ab5998a5fd10eb320c275
nmufson <nickmufson1@gmail.com>
2024-03-18 14:48:05 -0400

    Add assignments for operator and second input number

--- calculator-functions.js
+++ calculator-functions.js
@@ -26,21 +26,62 @@ function operate(operator,firstNumber,secondNumber) {
 
 const display = document.querySelector('#display')
 
-
-
 const buttonInput = document.querySelectorAll('.input');
+
+const buttonOperator = document.querySelectorAll('.operator')
 
 const buttonClear = document.querySelector('#clear')
 
 buttonInput.forEach((button) => {
     button.addEventListener('click', () => {
-        display.textContent = button.textContent;
+        if (operator === undefined) {
+            firstNumber = display.textContent
+            if ((display.textContent === '0') && (button.textContent === '.')) {
+                display.textContent += button.textContent;
+                firstNumber = display.textContent;
+            } else if (!(display.textContent === '0')) {
+                display.textContent += button.textContent;
+                firstNumber = display.textContent;
+            } else {
+                display.textContent = button.textContent;
+                firstNumber = display.textContent;
+            }
+        }
+
+        if (operator) {
+            if (secondNumber) {
+                display.textContent += button.textContent
+            } else {
+                display.textContent = button.textContent;
+            }
+            
+            secondNumber = display.textContent;
+
+
+        }
+        
     });
 });
+
+
+
+buttonOperator.forEach((button) => {
+    button.addEventListener('click', () => {
+        operator = button.textContent;
+    })
+})
+
+
 
 buttonClear.addEventListener('click', () => {
     display.textContent = 0;
 });
+
+
+
+
+
+
 
 
 
