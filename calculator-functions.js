d47e3b2df517d1fede0be95d01c21a3b9447483a
nmufson <nickmufson1@gmail.com>
2024-03-18 16:21:14 -0400

    Add equal sign func
    Add equal sign functionality and updated clear event listener to set input numbers and operator to undefined when clearing the calculator

--- calculator-functions.js
+++ calculator-functions.js
@@ -2,6 +2,7 @@
 
 let firstNumber;
 let operator;
+let operatorSymbol;
 let secondNumber;
 
 function add(a,b) {
@@ -21,15 +22,16 @@ function divide(a,b) {
 };
 
 function operate(operator,firstNumber,secondNumber) {
-    return operator(firstNumber,secondNumber);
+    if (operator === '+') return add(firstNumber,secondNumber);
+    if (operator === '-') return subtract(firstNumber,secondNumber);
+    if (operator === 'x') return multiply(firstNumber,secondNumber);
+    if (operator === '/') return divide(firstNumber,secondNumber);
 };
 
 const display = document.querySelector('#display')
-
 const buttonInput = document.querySelectorAll('.input');
-
 const buttonOperator = document.querySelectorAll('.operator')
-
+const buttonEquals = document.querySelector('#equals')
 const buttonClear = document.querySelector('#clear')
 
 buttonInput.forEach((button) => {
@@ -38,28 +40,21 @@ buttonInput.forEach((button) => {
             firstNumber = display.textContent
             if ((display.textContent === '0') && (button.textContent === '.')) {
                 display.textContent += button.textContent;
-                firstNumber = display.textContent;
             } else if (!(display.textContent === '0')) {
                 display.textContent += button.textContent;
-                firstNumber = display.textContent;
             } else {
                 display.textContent = button.textContent;
-                firstNumber = display.textContent;
             }
+            firstNumber = parseFloat(display.textContent);
         }
-
         if (operator) {
             if (secondNumber) {
                 display.textContent += button.textContent
             } else {
                 display.textContent = button.textContent;
             }
-            
-            secondNumber = display.textContent;
-
-
+            secondNumber = parseFloat(display.textContent);
         }
-        
     });
 });
 
@@ -71,11 +66,20 @@ buttonOperator.forEach((button) => {
     })
 })
 
+buttonEquals.addEventListener('click', () => {
+    display.textContent = operate(operator,firstNumber,secondNumber)
+})
+
 
 
 buttonClear.addEventListener('click', () => {
     display.textContent = 0;
+    firstNumber = undefined;
+    secondNumber = undefined;
+    operator = undefined;
 });
+
+
 
 
 
